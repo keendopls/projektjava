3841fce92556c377a47a2cbaf791b3d4bafbd3d4
Maziel <maziel99mz@gmail.com>
2020-05-15 14:04:20 +0200

    Utworzenie klasy Main

---
+++ projektjava/src/main/java/Main.java
@@ -0,0 +1,71 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Pomieszczenie[] mojePomieszczenia = {
+                new Pomieszczenie() {
+                    {
+                        this.szerokosc = 3;
+                        this.dlugosc = 2;
+                        this.nazwa = "pokoj";
+                    }
+                },
+                new Pomieszczenie() {
+                    {
+                        szerokosc = 2;
+                        dlugosc = 2;
+                        nazwa = "lazienka";
+                    }
+                }
+        };
+
+        Mieszkanie mojeMieszkanie = new Mieszkanie(){
+            {
+                pomieszczenia = mojePomieszczenia;
+                wizytowka = "Kowalscy";
+            }
+        };
+
+        ElementPietra[] mojeElementyParteru = {
+                mojeMieszkanie,
+                new Pomieszczenie(){
+                    {
+                        szerokosc = 2;
+                        dlugosc = 2;
+                        nazwa = "suszarnia";
+                    }
+                }
+        };
+
+        Pietro[] mojePietra = {
+                new Pietro(){
+                    {
+                        numerPietra = 0;
+                        elementy = mojeElementyParteru;
+                    }
+                }
+        };
+
+        Garaz[] mojeGaraze = {
+                new Garaz() {
+                    {
+                        szerokoscBramy = 3;
+                        wysokoscBramy = 2;
+                        szerokosc = 5;
+                        dlugosc = 5;
+                    }
+                }
+        };
+
+        Budynek mojBudynek = new Budynek(){
+            {
+                powierzchniaDzialki = 20;
+                pietra = mojePietra;
+                garaze = mojeGaraze;
+            }
+        };
+
+        System.out.println(mojBudynek.policzPole());
+
+    }
+}
+
